311c68d731d0392fbd24221eb9cb89547f6ca8f6
JeanVanneste <jeanvanneste@gmail.com>
2019-07-29 21:59:26 +0200

    Read numbers from files

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+src/generate_data.py
+cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug
---
+++ src/main.c
@@ -0,0 +1,103 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_MAX_LENGTH 6
+
+unsigned int count_lines(const char *filename);
+int* read_file_to_array(const char* filename, int size);
+int convert_array_to_int(char* number, int size);
+
+int main()
+{
+    const char* filename = "data";
+    unsigned int line_count = count_lines(filename);
+    printf("Nombre de lignes : %d\n", line_count);
+
+    
+    int *data = read_file_to_array(filename, line_count);
+    for (int i = 0; i < line_count; i++)
+    {
+        printf("%d\n", data[i]);
+    }   
+
+    /*
+    char *data = "285697";
+    int number = convert_array_to_int(data, 6);
+    printf("%d\n", number);
+     */
+    int max = 0; 
+
+    for (int i = 0; i < line_count; i++)
+    {
+        if (data[i] > max) { max = data[i]; }
+    }
+    printf("%d\n", max);
+}
+
+unsigned int count_lines(const char* filename)
+{
+    int file;
+    unsigned int line_count = 0;
+    if ((file = open(filename, O_RDONLY)) != -1)
+    {
+        char buffer[1];
+        ssize_t s;
+        while ((s = read(file, buffer, 1)) != 0)
+        {
+            if (*buffer == '\n')
+                line_count++;
+        }
+        close(file);
+    }
+
+    return line_count;
+}
+
+int* read_file_to_array(const char* filename, int size)
+{
+    int *data = malloc(size * sizeof(int));
+    int file;
+    if ((file = open(filename, O_RDONLY)) != -1)
+    {
+        char buffer[1];
+        ssize_t s;
+        char number[NUM_MAX_LENGTH];
+        int i = 0;
+        int k = 0;
+        while ((s = read(file, buffer, 1)) != 0)
+        {
+            if (*buffer == '\n')
+            {
+                data[i] = convert_array_to_int(number, k);
+                i++;
+                k = 0;
+            }
+            else
+            {
+                number[k] = (int)*buffer;
+                k++;
+            }
+        }
+    }
+    close(file);
+
+    return data;
+}
+
+int convert_array_to_int(char* number, int size)
+{
+    int mul = 1;
+    int result = 0;
+    for (int i = size - 1; i >= 0; i--)
+    {
+        result += (int)(number[i] - '0') * mul;
+        mul *= 10;
+    }
+    return result;
+}
