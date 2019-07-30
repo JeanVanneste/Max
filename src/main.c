b8cde0ecec8146934b7af4565022365daf41a619
JeanVanneste <jeanvanneste@gmail.com>
2019-07-30 18:16:56 +0200

    Write result to file

--- src/main.c
+++ src/main.c
@@ -4,11 +4,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/stat.h>
+
 #define NUM_MAX_LENGTH 6
 
 unsigned int count_lines(const char *filename);
 int* read_file_to_array(const char* filename, int size);
+void write_to_file(int max, const char* filename);
 int convert_array_to_int(char* number, int size);
+char* convert_int_to_array(int num);
 
 int main(int argc, char* argv[])
 {
@@ -26,14 +30,12 @@ int main(int argc, char* argv[])
     int *data = read_file_to_array(filename, line_count);
 
     int max = data[0];
-    int min = data[0];
     for (int i = 0; i < line_count; i++)
     {
         if (data[i] > max) { max = data[i]; }
-        if (data[i] < min) { min = data[i]; }
     }
-    printf("Nombre le plus petit : %d\n", min);
-    printf("Nombre le plus grand : %d\n", max);
+
+    write_to_file(max, argv[3]);
 
     free(data);
 }
@@ -88,6 +90,24 @@ int* read_file_to_array(const char* filename, int size)
     return data;
 }
 
+void write_to_file(int max, const char* filename)
+{
+    int file;
+    if ((file = open(filename, O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR | S_IROTH)) != -1)
+    {
+        char* max_str = convert_int_to_array(max);
+        write(STDOUT_FILENO, max_str, NUM_MAX_LENGTH);
+        write(file, max_str, NUM_MAX_LENGTH);
+        write(STDOUT_FILENO, "\n", 1);
+        write(file, "\n", 1);
+        free(max_str);
+    }
+    else
+    {
+        printf("Can't open destination file, check permissions\n");
+    }
+}
+
 int convert_array_to_int(char* number, int size)
 {
     int mul = 1;
@@ -98,4 +118,25 @@ int convert_array_to_int(char* number, int size)
         mul *= 10;
     }
     return result;
+}
+
+char* convert_int_to_array(int num)
+{
+    int size = 0;
+    int num_copy = num;
+    while (num_copy > 0)
+    {
+        num_copy /= 10;
+        size++;
+    }
+
+    char *array = calloc(size, sizeof(char));
+
+    for (int i = size - 1; i >= 0; i--)
+    {
+        array[i] = '0' + (num % 10);
+        num /= 10;
+    }
+
+    return array;
 }
